296620bfabe0067cd93c7ceea5ffb41a05d7b613
Jacob727272 <jacobmanoj2910@gmail.com>
2026-05-14 20:26:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 # ----------------------------
 # Random suffix for uniqueness
 # ----------------------------
