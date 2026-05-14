b964ed942f195a8ae082edf04f6bfa1391c859dd
Jacob727272 <jacobmanoj2910@gmail.com>
2026-05-14 13:48:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,43 @@
 resource "aws_security_group" "nginx_sg" {
-  name = "nginx-sg"
+
+  name = "nginx-security-group"
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx_server" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.nginx_sg.id
+  ]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = var.server_name
   }
 }
