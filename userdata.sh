94629b32338ed8e3f7ddf497bfad023a1800736f
Jacob727272 <jacobmanoj2910@gmail.com>
2026-05-14 13:49:15 +0530

    Update NGINX welcome message in userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 apt update -y
+
 apt install nginx -y
 
 systemctl enable nginx
+
 systemctl start nginx
 
-echo "<h1>NGINX Installed from Jenkins + Terraform</h1>" \
+echo "<h1>Welcome from Jenkins Terraform Pipeline</h1>" \
 > /var/www/html/index.html
