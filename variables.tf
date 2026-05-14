5285db19dbec7912a0312cf34e164281bdabc0dd
Jacob727272 <jacobmanoj2910@gmail.com>
2026-05-14 20:14:45 +0530

    Add default values to Terraform variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "aws_region" {
-  type = string
+  type    = string
+  default = "us-east-1"
 }
 
 variable "ami_id" {
-  type = string
+  type    = string
+  default = "ami-091138d0f0d41ff90"
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
+  default = "t3.micro"
 }
 
 variable "key_name" {
-  type = string
+  type    = string
+  default = "jacob"
 }
 
 variable "server_name" {
-  type = string
+  type    = string
+  default = "nginx-server"
 }
